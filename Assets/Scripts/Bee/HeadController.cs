24b299fc2ae6bd860def36828a11a39fd5e55ce6
Codatron <87000871+Codatron@users.noreply.github.com>
2022-09-09 15:41:04 +0200

    Updated movement and rotation

---
+++ Assets/Scripts/Bee/HeadController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    [SerializeField] private float xAxisRotationLimit;
+    [SerializeField] private float yAxisRotationLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+    }
+}
