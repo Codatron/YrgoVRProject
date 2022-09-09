63dfdd0044f45b94a2fcd60805a2e37e88390d9f
Codatron <87000871+Codatron@users.noreply.github.com>
2022-09-09 11:24:12 +0200

    BeeState enum script

--- Assets/Scripts/BeeState.cs
+++ Assets/Scripts/BeeState.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class BeeState : MonoBehaviour
+public enum BeeState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Flying,
+    NotFlying
 }
+
