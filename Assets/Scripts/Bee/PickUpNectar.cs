666b8be5128f78cef20985a96bfb37a5cc92a137
mayawingardh <mayaandersson@hotmail.se>
2022-09-20 11:06:15 +0200

    fix particall script

--- Assets/Scripts/Bee/PickUpNectar.cs
+++ Assets/Scripts/Bee/PickUpNectar.cs
@@ -7,12 +7,7 @@ public class PickUpNectar : MonoBehaviour
 {
     public SONectar sONectar;
     private float destroyTime;
-
-    [SerializeField]
-    private float maxSpeed;
-
     public Rigidbody playerRB;
-    public ParticleSystem nectarParticleSystem;
 
     private void Start()
     {
@@ -21,10 +16,6 @@ public class PickUpNectar : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Flower")) //&& playerRB.velocity.magnitude < maxSpeed) //ar det har ratt?
-        {
-            nectarParticleSystem.Play(); //Hur fa det att bara spela en gang, loser hastigheten det?
-        }
 
         if (other.gameObject.CompareTag("Nectar"))
         {
@@ -49,25 +40,6 @@ public class PickUpNectar : MonoBehaviour
     //private void LeaveNectar()
     //{
     //    sONectar.currentNectar = 0;
-    //}
-
-    //void GetParticalEffect()
-    //{
-
-    //    GameObject nectarPS = ObjectPoolPatricalEffectNectar.SharedInstance.GetPooledObject();
-
-    //    if (nectarPS != null)
-    //    {
-    //        //nectarPS.transform.position = turret.transform.postion;
-    //        //nectarPS.transform.rotation = turret.transform.rotation;
-    //        nectarPS.SetActiv();
-            
-
-    //    }
-
-
-    //    nectarPS.setActiv(false);
-
     //}
 
 }
---
+++ Assets/Scripts/PlayParticalEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticalEffect : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public ParticleSystem nectarParticleSystem;
+    [SerializeField]
+    private float maxSpeed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Flower")) //&& playerRB.velocity.magnitude < maxSpeed)
+            nectarParticleSystem.Play();
+        }
+    }
+}
