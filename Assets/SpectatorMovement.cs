53f2eab9a00d29c9bdd002de79ce4453ecc0e8d2
Markus <martve1214@skola.goteborg.se>
2022-09-28 14:29:57 +0200

    lighting stuff and optimization

--- Assets/SpectatorMovement.cs
+++ Assets/SpectatorMovement.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 
 public class SpectatorMovement : MonoBehaviour
 {
-    public AnimationCurve accelerationCurve;
-
-    private float movementSpeed;
-
+    public float movementSpeed;
     float horizontalInput;
     float verticalInput;
-    float speedPercentage;
 
     bool isFlying;
+    bool isDescending;
 
     Vector3 movementDirection;
-    Vector3 acceleration;
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
         isFlying = Input.GetKey(KeyCode.Space);
+        isDescending = Input.GetKey(KeyCode.LeftShift);
 
-        movementDirection = isFlying ? new Vector3(horizontalInput, 1, verticalInput) : new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection += isFlying ? new Vector3(horizontalInput, 1, verticalInput) : new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection += isDescending ? new Vector3(horizontalInput, -1, verticalInput) : new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
-
-        movementSpeed = accelerationCurve.Evaluate(movementDirection.magnitude);
 
         transform.position += movementDirection * movementSpeed * Time.deltaTime;
     }
